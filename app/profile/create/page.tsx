792290572742ec964b951148e9b331a3ce746eca
Dano <danocolombo@gmail.com>
2025-01-15 05:15:24 -0500

    now operational, no errors. time to move forward

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,9 +1,16 @@
-'use client';
+import React, { useEffect, useState } from 'react';
+import { GetServerSideProps } from 'next';
+import FormContainer from '@/components/form/FormContainer';
+import FormInput from '@/components/form/FormInput';
 import { currentUser } from '@clerk/nextjs/server';
-import { useUser } from '@clerk/clerk-react';
-import React from 'react';
+import { redirect } from 'next/navigation';
+import { createProfileAction, checkJerichoUser } from '@/utils/actions';
+import { useToast } from '@/hooks/use-toast';
+import { JerichoUserType } from '@/utils/types';
+import { fetchJerichoUser } from '@/providers/users';
+import { SubmitButton } from '@/components/form/Buttons';
+import axios from 'axios';
 import { SessionPayloadType } from '@/types/types';
-import { getAuthUser } from '@/utils/jericho';
 /*
 userId: string;
     clerkId: string;
@@ -22,6 +29,29 @@ userId: string;
 */
 export default async function CreateProfilePage() {
     const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+
+    //* ---------------------------------
+    //* get Clerk user data
+    //* ---------------------------------
+    const user = await currentUser();
+    // console.log('clerk_current_user\n', user);
+    const primaryEmailAddressId = user?.primaryEmailAddressId;
+    const clerkPrimaryEmailAddress = await user?.emailAddresses.find(
+        (email) => {
+            return email.id === primaryEmailAddressId;
+        }
+    );
+    let variables: SessionPayloadType = {
+        clerkId: user?.id,
+        userEmail: clerkPrimaryEmailAddress.emailAddress,
+        orgId: user?.privateMetadata.organization.id,
+        orgCode: user?.privateMetadata.organization.code,
+        orgName: user?.privateMetadata.organization.name,
+        orgRole: user?.privateMetadata.organization.role,
+        expiresAt: new Date(),
+    };
+    console.log('PCP:26--> variables:\n', variables);
+
     const res = await fetch(new URL('/api/session', baseUrl), {
         method: 'GET',
         headers: {
@@ -31,7 +61,7 @@ export default async function CreateProfilePage() {
     const data = await res.json();
     console.log('Response payload:', data);
     //Server side rendering
-    const user = await currentUser();
+    // const user = await currentUser();
     // const { isSignedIn, user, isLoaded } = await useUser();
     // console.log('PCP:27--> isSignedIn:\n', isSignedIn);
     // console.log('PCP:28--> user:\n', user);
