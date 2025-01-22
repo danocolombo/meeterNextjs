e404dbde4a967603cf6ecc1b38a0e3aac93a7503
danocolombo@google.com <GR0mans1212!>
2025-01-22 10:20:14 -0500

    profile improved

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -14,7 +14,7 @@ async function ProfilePage() {
     const clerkCurrentUser: any = await currentUser();
     const profileImage = await fetchProfileImage();
     const profile = clerkCurrentUser.privateMetadata.meeter;
-    console.log('ap15-->clerkCurrentUser:\n', clerkCurrentUser);
+    console.log('ap15-->profile:\n', profile);
     return (
         <section>
             <h1 className='text-2xl font-semibold mb-8 capitalize'>
@@ -43,21 +43,55 @@ async function ProfilePage() {
                         />
                         <FormInput
                             type='text'
-                            name='username'
-                            label='Username'
-                            defaultValue={profile.username}
+                            name='phone'
+                            label='Phone'
+                            defaultValue={profile.phone}
                         />
-                        <div className='flex flex-col p-2'>
-                            <Card className='text-sm text-gray-100 p-4'>
-                                <h2 className='text-lg font-semibold'>
-                                    Organization
-                                </h2>
-                                <div className='flex flex-col'>
-                                    <span>id: {profile.orgId}</span>
-                                    <span>code: {profile.orgCode}</span>
-                                    <span>name: {profile.orgName}</span>
-                                </div>
-                            </Card>
+                        <FormInput
+                            type='text'
+                            name='birthday'
+                            label='Birthday'
+                            defaultValue={profile.birthday}
+                        />
+                    </div>
+                    <h2 className='text-xl font-semibold mt-2'>
+                        System Information
+                    </h2>
+                    <div className='grid md:grid-cols-1 gap-4 '>
+                        <div className='flex p-2'>
+                            <div className='flex p-2 w-full'>
+                                <Card className='px-4 py-2 w-full'>
+                                    <div className='grid md:grid-cols-2 gap-4 text-gray-500'>
+                                        <div className='flex flex-col'>
+                                            <span className='text-lg font-semibold'>
+                                                Personal Information
+                                            </span>
+                                            <span>
+                                                username: {profile.username}
+                                            </span>
+                                            <span>email: {profile.email}</span>
+                                            <span>id: {profile.id}</span>
+                                            <span>
+                                                clerkId: {profile.clerkId}
+                                            </span>
+                                            <span>
+                                                jerichoId: {profile.jerichoId}
+                                            </span>
+                                            <span>
+                                                cognitoSub: {profile.sub}
+                                            </span>
+                                        </div>
+                                        <div className='flex flex-col'>
+                                            <span className='text-lg font-semibold'>
+                                                Organization
+                                            </span>
+                                            <span>id: {profile.orgId}</span>
+                                            <span>code: {profile.orgCode}</span>
+                                            <span>name: {profile.orgName}</span>
+                                        </div>
+                                    </div>
+                                </Card>
+                            </div>
                         </div>
                     </div>
                     <SubmitButton text='update profile' className='mt-8' />
