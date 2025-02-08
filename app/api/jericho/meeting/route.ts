69f51b5007ddd612430fdb43bf829af2c877a928
Dano <danocolombo@gmail.com>
2025-02-07 22:18:20 -0500

    comparing meetingForm changes/updates

--- app/api/jericho/meeting/route.ts
+++ app/api/jericho/meeting/route.ts
@@ -1,13 +1,13 @@
 import { NextResponse } from 'next/server';
 import axios from 'axios';
-import {type POST_DATA, type DB_DATA} from './types';
+import { type POST_DATA, type DB_DATA } from './types';
 import { type MeetingType } from '@/utils/types';
 import { MEETING_TYPE } from '@/utils/constants';
 
 export async function POST(request: Request) {
     const body = await request.json();
 
-    console.log('🟨 => route.ts:8 => POST body:', body);
+    console.log('🟨 => route.ts:10 => POST body:', body);
 
     // Process the received data (e.g., save to database)
 
@@ -30,25 +30,24 @@ export async function PUT(request: Request) {
             action: null,
         },
         groups: [],
-    }
-    if(body.groups) {
+    };
+    if (body.groups) {
         postData.groups = body.groups.map((group: any) => {
             return {
                 id: group?.id ? group?.id : null,
                 action: group?.id ? null : 'POST',
-            }
+            };
         });
     }
 
-    // console.log('🟨 => route.ts:35 => PUT body:', body);
-    // console.log('🟨 => route.ts:36 => postData:', postData);
+    // console.log('🟨 => route.ts:43 => PUT body:', body);
+    // console.log('🟨 => route.ts:44 => postData:', postData);
 
     //=================================================================================================
     // get the meeting from the database
     //=================================================================================================
-    let dbMeeting: MEETING_TYPE | null = null;
+    let dbMeeting: any | null = null;
     try {
-        
         const endpoint = `${process.env.NEXT_PUBLIC_JERICHO_API_ENDPOINT}/meeting/${body.organizationId}/${body.id}`;
         const response = await axios.get(endpoint, {
             headers: {
@@ -59,39 +58,73 @@ export async function PUT(request: Request) {
         let dbResponse: any = null;
         if (response?.data?.status === 200) {
             dbResponse = response.data.data;
-            dbMeeting = {...dbResponse};
+            dbMeeting = { ...dbResponse };
         } else {
             throw new Error('Failed to fetch meeting data');
         }
-
     } catch (error: any) {
-        console.log('🟨 => route.ts:67 => CATCH:', error);
+        console.log('🟨 => route.ts:66 => CATCH:', error);
         const errorMessage =
             error.response?.data?.message ||
             error.message ||
             'An error occurred';
-        return NextResponse.json({ status: 500, message: errorMessage, data: {id: null} });
+        return NextResponse.json({
+            status: 500,
+            message: errorMessage,
+            data: { id: null },
+        });
     }
 
     //=================================================================================================
     // set the DB_DATA
     //=================================================================================================
-    console.log('🟨 => route.ts:79 => dbMeeting:', dbMeeting);
+    // console.log('🟨 => route.ts:81 => dbMeeting:', dbMeeting);
     const dbData: DB_DATA = {
         groups: [],
-    }
-    if(dbMeeting?.groups) {
+    };
+    if (dbMeeting?.groups) {
         dbData.groups = dbMeeting?.groups.map((group: any) => {
             return {
-                id:group?.id,
-                action: null
-            }
+                id: group?.id,
+                action: null,
+            };
         });
     }
-    console.log('🟨 => route.ts:91 => postData:', postData);
-    console.log('🟨 => route.ts:79 => dbData:', dbData);
-    // // Process the received data (e.g., save to database)
 
+    // check if the meeting has changed
+    let postCompare: any = { ...body };
+    delete postCompare.groups;
+    delete postCompare.organizationId;
+    delete postCompare.apiToken;
+    if (postCompare?.facilitator_contact == '') {
+        postCompare.facilitator_contact = null;
+    }
+    if (postCompare?.support_contact == '') {
+        postCompare.support_contact = null;
+    }
+    if (postCompare?.attendance_count == 0) {
+        postCompare.attendance_count = null;
+    }
+    if (postCompare?.meal_count == 0) {
+        postCompare.meal_count = null;
+    }
+    if (postCompare?.newcomers_count == 0) {
+        postCompare.newcomers_count = null;
+    }
+    let dbCompare: any = { ...dbMeeting };
+    delete dbCompare.groups;
+
+    const hasChanges = Object.keys(postCompare).some(
+        (key) =>
+            JSON.stringify(postCompare[key]) !== JSON.stringify(dbCompare[key])
+    );
+    if (hasChanges) {
+        postData.meeting.action = 'PUT';
+    }
+    console.log('🟨 => route.ts:126 => hasChanges:', hasChanges);
+    console.log('🟨 => route.ts:127 => postData:', postData);
+    console.log('🟨 => route.ts:128 => dbData:', dbData);
+    postData.meeting.action = hasChanges ? 'PUT' : null;
     return NextResponse.json({
         status: 200,
         message: 'Meeting PUT saved successfully',
